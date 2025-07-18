bd1927d4fdd4fdd88bfe46dcc07ddb56305c2ecc
Aman Singh <aman.singh@infor.com>
2025-07-18 12:34:52 +0000

    Add commands to set a Terraform var on a group or workspace

---
+++ internal/command/workspace_delete_terraform_var.go
@@ -0,0 +1,136 @@
+package command
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mitchellh/cli"
+	"gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-cli/internal/optparser"
+	"gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-cli/internal/output"
+	tharsis "gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-sdk-go/pkg"
+	sdktypes "gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-sdk-go/pkg/types"
+)
+
+// workspaceDeleteTerraformVarCommand is the top-level structure for the workspace delete-terraform-var command.
+type workspaceDeleteTerraformVarCommand struct {
+	meta *Metadata
+}
+
+// NewWorkspaceDeleteTerraformVarCommandFactory returns a workspaceDeleteTerraformVarCommand struct.
+func NewWorkspaceDeleteTerraformVarCommandFactory(meta *Metadata) func() (cli.Command, error) {
+	return func() (cli.Command, error) {
+		return workspaceDeleteTerraformVarCommand{
+			meta: meta,
+		}, nil
+	}
+}
+
+func (wdv workspaceDeleteTerraformVarCommand) Run(args []string) int {
+	wdv.meta.Logger.Debugf("Starting the 'workspace delete-terraform-var' command with %d arguments:", len(args))
+	for ix, arg := range args {
+		wdv.meta.Logger.Debugf("    argument %d: %s", ix, arg)
+	}
+
+	// Cannot delay reading settings past this point.
+	settings, err := wdv.meta.ReadSettings()
+	if err != nil {
+		wdv.meta.Logger.Error(output.FormatError("failed to read settings file", err))
+		return 1
+	}
+
+	client, err := settings.CurrentProfile.GetSDKClient()
+	if err != nil {
+		wdv.meta.UI.Error(output.FormatError("failed to get SDK client", err))
+		return 1
+	}
+
+	ctx := context.Background()
+
+	return wdv.doWorkspaceDeleteTerraformVar(ctx, client, args)
+}
+
+func (wdv workspaceDeleteTerraformVarCommand) doWorkspaceDeleteTerraformVar(ctx context.Context, client *tharsis.Client, opts []string) int {
+	wdv.meta.Logger.Debugf("will do workspace delete-terraform-var, %d opts", len(opts))
+
+	defs := wdv.buildWorkspaceDeleteTerraformVarDefs()
+
+	cmdOpts, cmdArgs, err := optparser.ParseCommandOptions(wdv.meta.BinaryName+" workspace delete-terraform-var", defs, opts)
+	if err != nil {
+		wdv.meta.Logger.Error(output.FormatError("failed to parse workspace delete-terraform-var options", err))
+		return 1
+	}
+	if len(cmdArgs) < 1 {
+		wdv.meta.Logger.Error(output.FormatError("missing workspace delete-terraform-var workspace path", nil), wdv.HelpWorkspaceDeleteTerraformVar())
+		return 1
+	}
+	if len(cmdArgs) > 1 {
+		msg := fmt.Sprintf("excessive workspace delete-terraform-var arguments: %s", cmdArgs)
+		wdv.meta.Logger.Error(output.FormatError(msg, nil), wdv.HelpWorkspaceDeleteTerraformVar())
+		return 1
+	}
+
+	namespacePath := cmdArgs[0]
+	key := getOption("key", "", cmdOpts)[0]
+
+	if key == "" {
+		wdv.meta.Logger.Error(output.FormatError("missing required --key option", nil), wdv.HelpWorkspaceDeleteTerraformVar())
+		return 1
+	}
+
+	if !isNamespacePathValid(wdv.meta, namespacePath) {
+		return 1
+	}
+
+	if _, err = client.Workspaces.GetWorkspace(ctx, &sdktypes.GetWorkspaceInput{
+		Path: &namespacePath,
+	}); err != nil {
+		wdv.meta.Logger.Error(output.FormatError("failed to get workspace", err))
+		return 1
+	}
+
+	input := &sdktypes.DeleteNamespaceVariableInput{
+		ID: newResourceTRN("variable", namespacePath, string(sdktypes.TerraformVariableCategory), key),
+	}
+
+	wdv.meta.Logger.Debugf("workspace delete-terraform-var input: %#v", input)
+
+	err = client.Variable.DeleteVariable(ctx, input)
+	if err != nil {
+		wdv.meta.Logger.Error(output.FormatError("failed to delete workspace variable", err))
+		return 1
+	}
+
+	wdv.meta.UI.Output(fmt.Sprintf("Terraform variable '%s' deleted successfully from workspace %s", key, namespacePath))
+	return 0
+}
+
+func (wdv workspaceDeleteTerraformVarCommand) buildWorkspaceDeleteTerraformVarDefs() optparser.OptionDefinitions {
+	return optparser.OptionDefinitions{
+		"key": {
+			Arguments: []string{"Variable_Key"},
+			Synopsis:  "The key/name of the terraform variable to delete.",
+			Required:  true,
+		},
+	}
+}
+
+func (wdv workspaceDeleteTerraformVarCommand) Synopsis() string {
+	return "Delete a single terraform variable from a workspace."
+}
+
+func (wdv workspaceDeleteTerraformVarCommand) Help() string {
+	return wdv.HelpWorkspaceDeleteTerraformVar()
+}
+
+// HelpWorkspaceDeleteTerraformVar produces the help string for the 'workspace delete-terraform-var' command.
+func (wdv workspaceDeleteTerraformVarCommand) HelpWorkspaceDeleteTerraformVar() string {
+	return fmt.Sprintf(`
+Usage: %s [global options] workspace delete-terraform-var [options] <workspace>
+
+   The workspace delete-terraform-var command deletes a single terraform
+   variable from a workspace.
+
+%s
+
+`, wdv.meta.BinaryName, buildHelpText(wdv.buildWorkspaceDeleteTerraformVarDefs()))
+}
---
+++ internal/command/workspace_set_terraform_var.go
@@ -0,0 +1,193 @@
+package command
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mitchellh/cli"
+	"gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-cli/internal/optparser"
+	"gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-cli/internal/output"
+	tharsis "gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-sdk-go/pkg"
+	sdktypes "gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-sdk-go/pkg/types"
+)
+
+// workspaceSetTerraformVarCommand is the top-level structure for the workspace set-terraform-var command.
+type workspaceSetTerraformVarCommand struct {
+	meta *Metadata
+}
+
+// NewWorkspaceSetTerraformVarCommandFactory returns a workspaceSetTerraformVarCommand struct.
+func NewWorkspaceSetTerraformVarCommandFactory(meta *Metadata) func() (cli.Command, error) {
+	return func() (cli.Command, error) {
+		return workspaceSetTerraformVarCommand{
+			meta: meta,
+		}, nil
+	}
+}
+
+func (wsv workspaceSetTerraformVarCommand) Run(args []string) int {
+	wsv.meta.Logger.Debugf("Starting the 'workspace set-terraform-var' command with %d arguments:", len(args))
+	for ix, arg := range args {
+		wsv.meta.Logger.Debugf("    argument %d: %s", ix, arg)
+	}
+
+	// Cannot delay reading settings past this point.
+	settings, err := wsv.meta.ReadSettings()
+	if err != nil {
+		wsv.meta.Logger.Error(output.FormatError("failed to read settings file", err))
+		return 1
+	}
+
+	client, err := settings.CurrentProfile.GetSDKClient()
+	if err != nil {
+		wsv.meta.UI.Error(output.FormatError("failed to get SDK client", err))
+		return 1
+	}
+
+	ctx := context.Background()
+
+	return wsv.doWorkspaceSetTerraformVar(ctx, client, args)
+}
+
+func (wsv workspaceSetTerraformVarCommand) doWorkspaceSetTerraformVar(ctx context.Context, client *tharsis.Client, opts []string) int {
+	wsv.meta.Logger.Debugf("will do workspace set-terraform-var, %d opts", len(opts))
+
+	defs := wsv.buildWorkspaceSetTerraformVarDefs()
+
+	cmdOpts, cmdArgs, err := optparser.ParseCommandOptions(wsv.meta.BinaryName+" workspace set-terraform-var", defs, opts)
+	if err != nil {
+		wsv.meta.Logger.Error(output.FormatError("failed to parse workspace set-terraform-var options", err))
+		return 1
+	}
+	if len(cmdArgs) < 1 {
+		wsv.meta.Logger.Error(output.FormatError("missing workspace set-terraform-var workspace path", nil), wsv.HelpWorkspaceSetTerraformVar())
+		return 1
+	}
+	if len(cmdArgs) > 1 {
+		msg := fmt.Sprintf("excessive workspace set-terraform-var arguments: %s", cmdArgs)
+		wsv.meta.Logger.Error(output.FormatError(msg, nil), wsv.HelpWorkspaceSetTerraformVar())
+		return 1
+	}
+
+	namespacePath := cmdArgs[0]
+	key := getOption("key", "", cmdOpts)[0]
+	value := getOption("value", "", cmdOpts)[0]
+
+	if key == "" {
+		wsv.meta.Logger.Error(output.FormatError("missing required --key option", nil), wsv.HelpWorkspaceSetTerraformVar())
+		return 1
+	}
+	if value == "" {
+		wsv.meta.Logger.Error(output.FormatError("missing required --value option", nil), wsv.HelpWorkspaceSetTerraformVar())
+		return 1
+	}
+
+	sensitive, err := getBoolOptionValue("sensitive", "false", cmdOpts)
+	if err != nil {
+		wsv.meta.UI.Error(output.FormatError("failed to parse boolean value for --sensitive", err))
+		return 1
+	}
+
+	if !isNamespacePathValid(wsv.meta, namespacePath) {
+		return 1
+	}
+
+	if _, err = client.Workspaces.GetWorkspace(ctx, &sdktypes.GetWorkspaceInput{
+		Path: &namespacePath,
+	}); err != nil {
+		wsv.meta.Logger.Error(output.FormatError("failed to get workspace", err))
+		return 1
+	}
+
+	getInput := &sdktypes.GetNamespaceVariableInput{
+		ID: newResourceTRN("variable", namespacePath, string(sdktypes.TerraformVariableCategory), key),
+	}
+
+	wsv.meta.Logger.Debugf("workspace set-terraform-var get variable input: %#v", getInput)
+
+	variable, err := client.Variable.GetVariable(ctx, getInput)
+	if err != nil && !tharsis.IsNotFoundError(err) {
+		wsv.meta.Logger.Error(output.FormatError("failed to get variable", err))
+		return 1
+	}
+
+	if variable != nil {
+		if variable.Sensitive != sensitive {
+			wsv.meta.Logger.Error(output.FormatError("cannot change sensitive flag - delete and recreate the variable instead", nil))
+			return 1
+		}
+
+		updateInput := &sdktypes.UpdateNamespaceVariableInput{
+			ID:    variable.Metadata.ID,
+			Key:   variable.Key,
+			Value: value,
+		}
+
+		wsv.meta.Logger.Debugf("workspace set-terraform-var update variable input: %#v", updateInput)
+
+		if _, err = client.Variable.UpdateVariable(ctx, updateInput); err != nil {
+			wsv.meta.Logger.Error(output.FormatError("failed to update variable", err))
+			return 1
+		}
+	} else {
+		createInput := &sdktypes.CreateNamespaceVariableInput{
+			Key:           key,
+			Value:         value,
+			Category:      sdktypes.TerraformVariableCategory,
+			NamespacePath: namespacePath,
+			Sensitive:     sensitive,
+		}
+
+		wsv.meta.Logger.Debugf("workspace set-terraform-var create variable input: %#v", createInput)
+
+		if _, err = client.Variable.CreateVariable(ctx, createInput); err != nil {
+			wsv.meta.Logger.Error(output.FormatError("failed to create variable", err))
+			return 1
+		}
+	}
+
+	wsv.meta.UI.Output(fmt.Sprintf("Terraform variable '%s' set successfully in workspace %s", key, namespacePath))
+	return 0
+}
+
+func (wsv workspaceSetTerraformVarCommand) buildWorkspaceSetTerraformVarDefs() optparser.OptionDefinitions {
+	return optparser.OptionDefinitions{
+		"key": {
+			Arguments: []string{"Variable_Key"},
+			Synopsis:  "The key/name of the terraform variable.",
+			Required:  true,
+		},
+		"value": {
+			Arguments: []string{"Variable_Value"},
+			Synopsis:  "The value of the terraform variable.",
+			Required:  true,
+		},
+		"sensitive": {
+			Arguments: []string{},
+			Synopsis:  "Mark the variable as sensitive.",
+		},
+	}
+}
+
+func (wsv workspaceSetTerraformVarCommand) Synopsis() string {
+	return "Set a single terraform variable for a workspace."
+}
+
+func (wsv workspaceSetTerraformVarCommand) Help() string {
+	return wsv.HelpWorkspaceSetTerraformVar()
+}
+
+// HelpWorkspaceSetTerraformVar produces the help string for the 'workspace set-terraform-var' command.
+func (wsv workspaceSetTerraformVarCommand) HelpWorkspaceSetTerraformVar() string {
+	return fmt.Sprintf(`
+Usage: %s [global options] workspace set-terraform-var [options] <workspace>
+
+   The workspace set-terraform-var command sets a single terraform
+   variable for a workspace. If the variable already exists, it will
+   be deleted and recreated with the new value. Use the --sensitive
+   flag to mark the variable as sensitive.
+
+%s
+
+`, wsv.meta.BinaryName, buildHelpText(wsv.buildWorkspaceSetTerraformVarDefs()))
+}
